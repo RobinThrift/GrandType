5aeaf660ba66a87340d01597f168747c0aff84a6
Robin Thrift <thrift.d.robin@googlemail.com>
2015-05-04 14:40:17 +0200

    added basic files

---
+++ src/scripts/main.jsx
@@ -0,0 +1,17 @@
+// src/scripts/main.jsx
+
+import React from 'react';
+
+class Startpage extends React.Component {
+    render() {
+        return (
+            <div className="main">
+            </div>
+        );
+    }
+}
+
+React.render(
+    <Startpage />,
+    document.getElementById('react-render-target1')
+);
